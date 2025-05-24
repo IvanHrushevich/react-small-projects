184c01b96d0c49900335ad4d2367317bc2f49eb3
Ivan Grushevich <ivan.grushevich1@gmail.com>
2025-05-24 13:02:51 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router';
+
 function App() {
-    return <div className="text-3xl text-cyan-800 text-center">App</div>;
+    return (
+        <div className="min-h-screen bg-gray-50 p-8">
+            <div className="max-w-4xl mx-auto">
+                <h1 className="text-3xl font-bold text-gray-800 mb-8">
+                    Small React projects
+                </h1>
+
+                <div className="mb-8 space-x-4">
+                    <Link
+                        to="/kanban-board"
+                        className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
+                    >
+                        Kanban board
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
+import KanbanBoard from './pages/kanban-board/KanbanBoard.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/kanban-board" element={<KanbanBoard />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </BrowserRouter>
     </StrictMode>,
 );
